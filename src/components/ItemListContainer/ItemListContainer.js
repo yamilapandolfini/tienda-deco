e8bc514f423ab7217106c76c6138d126e37a1738
yamila.pandolfini <yamila.pandolfini@gmail.com>
2021-08-24 23:05:55 -0300

    entrega 6-modificada

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,12 +2,12 @@ import React, {useEffect, useState} from 'react'
 import {ItemList} from './ItemList'
 
 const datos = [ 
-    { id:1,title:'Remeras-desafio', description:'Remeras Rojas',        price:25,  pictureUrl:'' },
-    { id:2,title:'Pantalon-after', description:'Pantalon de jean',    price:35,  pictureUrl:'' },
-    { id:3,title:'Medias-after', description:'Medias con dibujitos',  price:500, pictureUrl:'' },
-    { id:4,title:'Remeras-entrega', description:'Remeras Rojas',        price:25,  pictureUrl:'' },
-    { id:5,title:'Pantalon-entrega', description:'Pantalon de jean',    price:35,  pictureUrl:'' },
-    { id:6,title:'Medias-entrega', description:'Medias con dibujitos',  price:500, pictureUrl:'' },
+    { id:1,title:'producto1', description:'Descripcion producto1', price:2500,  pictureUrl:'' },
+    { id:2,title:'producto2', description:'Descripcion producto2', price:3500,  pictureUrl:'' },
+    { id:3,title:'producto3', description:'Descripcion producto3', price:5000, pictureUrl:'' },
+    { id:4,title:'producto4', description:'Descripcion producto4', price:2552,  pictureUrl:'' },
+    { id:5,title:'producto5', description:'Descripcion producto5', price:3400,  pictureUrl:'' },
+    { id:6,title:'producto6', description:'Descripcion producto6', price:5000, pictureUrl:'' },
 ]
 
 const ItemListContainer = () =>{
